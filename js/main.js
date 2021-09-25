6a2fbd9deaa95d29c2ec767171b94688e170e041
Olga Chernikova <darkred2694@gmail.com>
2021-09-26 02:17:35 +0300

    Объединяет две функции в одну

--- js/main.js
+++ js/main.js
@@ -1,22 +1,8 @@
-function getRandomInt(min, max) {
-  min = Math.abs(Math.ceil(min));
-  max = Math.abs(Math.floor(max));
-  if(min>=max) {
-    return 'Введите корректный диапозон положительных чисел, начиная с наименьшего числа.';
+const getRandomInt = (min, max, length = 0) => {
+  if (min >= max || min < 0 || max <= 0) {
+    return false;
   }
-  return Math.floor(Math.random()*(max - min + 1)) + min;
-}
+  return (Math.random() * (max - min) + min).toFixed(length);
+};
 
-getRandomInt(0, 11);
-
-function getRandomFloatInt(min, max, lenght) {
-  min = Math.abs(+min.toFixed(1));
-  max = Math.abs(+max.toFixed(1));
-  if(min>=max) {
-    return 'Введите корректный диапозон положительных чисел, начиная с наименьшего числа.';
-  }
-  const int = Math.random()*(max - min) + min;
-  return +int.toFixed(lenght);
-}
-
-getRandomFloatInt(1.6, 3.555, 2);
+getRandomInt (1, 6);
