8ef9400cff61acd9e0d7f015d257beb991fb9058
Olga Chernikova <darkred2694@gmail.com>
2021-10-07 16:23:26 +0300

    Добавляет функцию для генерации массива из 10 случайных объектов

--- js/main.js
+++ js/main.js
@@ -1,3 +1,60 @@
+
+const TITLES = [
+  'Большой уютный номер',
+  'Маленькая функциональная квартира',
+  'Небольшой уютный бунгало',
+  'Трехэтажный особняк с бассейном',
+  'Гостевой дом с открытой верандой',
+  'Квартира в уютном районе Токио',
+  'Квартира с панорамным видом на город',
+  'Дизайнерская квартира-студия',
+  'Дом с прекрасным садом',
+  'Апартаменты с роскошным интерьером',
+];
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const TIME = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const DESCRIPTIONS = [
+  'До метро и вокзала буквально 4 минуты пешком. Рядом круглосуточный магазин, аптека. Поблизости много ресторанов, кафе.',
+  'Есть все необходимое для проживания. Недавно был сделан ремонт, так что в действительности даже лучше, чем на фото.',
+  'Все продумано до мелочей. Есть все условия для комфортного проживания. Спокойный жилой район, рядом метро и парки.',
+  'Прекрасное расположение, уютный и тихий двор. Всё продумано до мелочей!',
+  'Удобное расположение, бесконтактное заселение. Есть все для кратковременного проживания.',
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const ADS_LIST_LENGTH = 10;
+
+const DECIMALS = 5;
+
+const PAHT = 'img/avatars/user';
+
 const getRandomInt = (min, max, length = 0) => {
   if (min >= max || min < 0 || max <= 0) {
     return false;
@@ -5,4 +62,88 @@ const getRandomInt = (min, max, length = 0) => {
   return (Math.random() * (max - min) + min).toFixed(length);
 };
 
-getRandomInt (1, 6);
+const shuffle = (array) => {
+  let currentIndex = array.length,  randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+};
+
+const createAvatarPathArray = () => {
+  const array = [];
+  for (let index = 1; index <= ADS_LIST_LENGTH; index++) {
+    if (index < ADS_LIST_LENGTH) {
+      index = `0${index}`;
+    }
+    array.push(`${PAHT}${index}.png`);
+  }
+  return shuffle(array);
+};
+
+const pathArray = createAvatarPathArray();
+
+const getRandomElement = (array) => {
+  const elementIndex = getRandomInt(0, array.length - 1);
+  const randomElement = array[elementIndex];
+
+  array.slice(elementIndex, 1);
+  return randomElement;
+};
+
+const createRandomArray = (array) => {
+  const randomArray = [];
+  const copiedArray = array.slice();
+  const arrayLength = getRandomInt(1, array.length);
+
+  for (let index = 0; index < arrayLength; index++) {
+    const arrayElement = getRandomElement(copiedArray);
+    randomArray.push(arrayElement);
+  }
+
+  return Array.from(new Set(randomArray));
+};
+
+const createNewAd = (index) => {
+  const checkTime = TIME[getRandomInt(0, TIME.length - 1)];
+  const location = {
+    lat: getRandomInt(35.65000, 35.70000, DECIMALS),
+    lng: getRandomInt(139.70000, 139.80000, DECIMALS),
+  };
+
+  return {
+    author: {
+      avatar: pathArray[index],
+    },
+    offer: {
+      title: TITLES[getRandomInt(0, TITLES.length - 1)],
+      address: `${location.lat}, ${location.lng}`,
+      price: getRandomInt(0, 1000000),
+      type: TYPES[getRandomInt(0, TYPES.length - 1)],
+      rooms: getRandomInt(1, 10),
+      guests: getRandomInt(1, 10),
+      checkin: checkTime,
+      checkout: checkTime,
+      features: createRandomArray(FEATURES),
+      description: DESCRIPTIONS[getRandomInt(0, DESCRIPTIONS.length - 1)],
+      photos: createRandomArray(PHOTOS),
+    },
+    location,
+  };
+};
+
+const getAdsArray = () => {
+  const adsList = [];
+  for(let index = 0; index < ADS_LIST_LENGTH; index++) {
+    adsList.push(createNewAd(index));
+  }
+  return adsList;
+};
+
+getAdsArray();
